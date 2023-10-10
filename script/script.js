8022fb59a67aa241f4b29e9932ac59e2ae0c649c
shehryarlodhi <lodhishehryarkhan@gmail.com>
2023-10-10 22:17:12 +0500

    ...

--- script/script.js
+++ script/script.js
@@ -87,20 +87,23 @@ function AddFilter(filter_name) {
 
 
 function RemoveFilter(filter_name) {
-    const filterItem = $(`.${filter_name}`);
+  const filterItem = $(`.${filter_name}`);
 
-    // Remove the filter element from the filter bar
-    filterItem.remove();
+  // Remove the filter element from the filter bar
+  filterItem.remove();
 
-    // Remove the filter from the 'filters' array
-    const index = filters.indexOf(filter_name);
-    if (index !== -1) {
-        filters.splice(index, 1);
-    }
+  // Remove the filter from the 'filters' array
+  const index = filters.indexOf(filter_name);
+  if (index !== -1) {
+    filters.splice(index, 1);
+  }
 
-    // After removing the filter, update the displayed job listings
-    updateJobListings();
+  // After removing the filter, update the displayed job listings
+  updateJobListings(); // Add this line to update job listings
+
+  // Rest of your code...
 }
+
 
 
 
@@ -230,6 +233,10 @@ function getJobs(jobs) {
     
 }
 
+// ... Your existing code ...
+
+// Rest of your existing code...
+
 function openAddJobPopup() {
   const popup = $("<div id='addJobPopup' class='popup'></div>");
 
@@ -248,46 +255,83 @@ function openAddJobPopup() {
   form.append("<label for='level'>Level:</label>");
   form.append("<input type='text' id='level' required>");
 
-  form.append("<label for='languages'>Languages:</label>");
-  form.append("<input type='text' id='languages' placeholder='Comma-separated values'>");
+  form.append("<label for='languages'>Languages (comma-separated):</label>");
+  form.append("<input type='text' id='languages' required>");
 
-  form.append("<label for='tools'>Tools:</label>");
-  form.append("<input type='text' id='tools' placeholder='Comma-separated values'>");
+  form.append("<label for='tools'>Tools (comma-separated):</label>");
+  form.append("<input type='text' id='tools' required>");
 
   form.append("<label for='new'>New Job:</label>");
   form.append("<input type='checkbox' id='new'>");
 
   form.append("<button type='submit'>Add Job</button>");
+  form.append("<button type='button' id='cancelButton'>Cancel</button>");
 
   form.on("submit", function (event) {
     event.preventDefault();
 
-    const newJob = {
-      company: $("#company").val(),
-      position: $("#position").val(),
-      role: $("#role").val(),
-      level: $("#level").val(),
-      languages: $("#languages").val().split(",").map((language) => language.trim()),
-      tools: $("#tools").val().split(",").map((tool) => tool.trim()),
-      new: $("#new").prop("checked"),
-    };
+    // Get values from form inputs
+    const company = $("#company").val();
+    const position = $("#position").val();
+    const role = $("#role").val();
+    const level = $("#level").val();
+    const languages = $("#languages").val().split(",").map((language) => language.trim());
+    const tools = $("#tools").val().split(",").map((tool) => tool.trim());
+    const newJob = $("#new").prop("checked");
 
     // Add validations as needed
-    if (!newJob.company || !newJob.position || !newJob.role || !newJob.level) {
-      alert("Please fill in all required fields (Company, Position, Role, Level).");
+    if (!company || !position || !role || !level || !languages.length || !tools.length) {
+      alert("Please fill in all required fields (Company, Position, Role, Level, Languages, and Tools).");
       return;
     }
 
+    // Create a new job object based on user input
+    const newJobObject = {
+      company: company,
+      position: position,
+      role: role,
+      level: level,
+      languages: languages,
+      tools: tools,
+      new: newJob,
+    };
+
     // Add the new job to the data array
-    data.push(newJob);
+    data.push(newJobObject);
 
     // Close the pop-up
     popup.remove();
 
     // Update the job listings with the new data
     updateJobListings();
+
+    // Save the updated data to data.json
+    saveDataToFile(data);
   });
 
   popup.append(form);
   $("body").append(popup);
+
+  // Clicking the "Cancel" button hides the form
+  $("#cancelButton").click(function () {
+    popup.remove();
+  });
+}
+
+function saveDataToFile(data) {
+  const jsonData = JSON.stringify(data, null, 2); // Convert data to JSON string with pretty formatting
+
+  const blob = new Blob([jsonData], { type: 'application/json' });
+  const url = URL.createObjectURL(blob);
+
+  // Create a hidden <a> element using jQuery and trigger a click event to download the JSON file
+  const $a = $('<a>', {
+    href: url,
+    download: 'data.json'
+  }).appendTo('body');
+  $a[0].click(); // Trigger the click event on the first (and only) element in the jQuery collection
+
+  // Clean up by revoking the object URL and removing the <a> element
+  URL.revokeObjectURL(url);
+  $a.remove();
 }
